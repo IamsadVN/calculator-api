0b4ee87cd29ccaefc7570ccd774fef6ef7b9118a
IamsadVN <112055678+IamsadVN@users.noreply.github.com>
2025-06-20 11:07:02 +0700

    enhance validation and support 4th degree polynomial for 'polynomial' router

--- src/controllers/equation/polynomial.ts
+++ src/controllers/equation/polynomial.ts
@@ -1,20 +1,68 @@
 import { Request, Response } from "express";
 import { PolynomialRequestBody } from "../../types/index.js";
-import * as math from "mathjs";
+import { solveLinear, solveQuadratic, solveCubic, solveQuartic } from "@littlefattie/solve-equations";
 
 export function polynomial(req: Request<{}, {}, PolynomialRequestBody>, res: Response) {
     const { coeffs } = req.body;
 
-    if (coeffs.length > 4) {
-        res.status(400).json({error: "4th degree polynomials not supported yet"});
+    if (!coeffs) {
+        res.json({
+            error: "'coeffs' is required"
+        });
         return;
     }
-    else if (coeffs.length < 3) {
-        res.status(400).json({error: "A Polynomial must be have 2 or more degree"});
+    if (!Array.isArray(coeffs)) {
+        res.json({
+            error: "'coeffs' must be an array and contain numbers"
+        });
         return;
     }
+    for (const element of coeffs) {
+        if (typeof element !== "number") {
+            res.json({
+                error: `Element "${element}" is not a number`
+            });
+            return;
+        }
+    }
 
-    const result = math.polynomialRoot(...coeffs);
+    switch (coeffs.length) {
+        case 2: {
+            res.json({
+                result: solveLinear(coeffs[0],coeffs[1])
+            });
+            return;
+        }
+        case 3: {
+            res.json({
+                result: solveQuadratic(coeffs[0],coeffs[1],coeffs[2]!)
+            });
+            return;
+        }
+        case 4: {
+            res.json({
+                result: solveCubic(coeffs[0],coeffs[1],coeffs[2]!,coeffs[3]!)
+            });
+            return;
+        }
+        case 5: {
+            res.json({
+                result: solveQuartic(coeffs[0],coeffs[1],coeffs[2]!,coeffs[3]!,coeffs[4]!)
+            });
+            return;
+        }
+        default: {
+            if (coeffs.length < 2) {
+                res.json({error: "'coeffs' must have between 2 to 5 elements"});
+                return;
+            }
+            if (coeffs.length > 5) {
+                res.json({error: "'coeffs' must not have over 5 elements"});
+                return;
+            }
 
-    res.json({result});
+            break;
+        }
+    }
+
 }
